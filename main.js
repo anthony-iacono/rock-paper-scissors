0a033540a03a1329bd52420f173f5014e0ed5cbf
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-08-07 16:09:09 -0700

    Update default message

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const game = new Game;
-const classicBtn = document.querySelector('.js-classic-mode');
-const advancedBtn = document.querySelector('.js-advanced-mode');
+const classicBtn = document.querySelector('.js-classic-btn');
+const advancedBtn = document.querySelector('.js-advanced-btn');
 
 let humanWinsCounter = document.querySelector('.js-human-wins-counter');
 let computerWinsCounter = document.querySelector('.js-computer-wins-counter');
@@ -13,5 +13,5 @@ classicBtn.addEventListener('click', startClassicGame);
 function startClassicGame() {
   game.type = 'classic';
   game.message = 'Choose your fighter!';
-  
+
 }
