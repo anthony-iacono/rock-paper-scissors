6b97c0f723f775595d131d89cd902374f718a79f
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-08-07 11:54:54 -0700

    Add start classic game event listener

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ const advancedBtn = document.querySelector('.js-advanced-mode');
 
 let humanWinsCounter = document.querySelector('.js-human-wins-counter');
 let computerWinsCounter = document.querySelector('.js-computer-wins-counter');
+
+humanWinsCounter.innerText = game.human.wins;
+computerWinsCounter.innerText = game.computer.wins;
+
+classicBtn.addEventListener('click', startClassicGame);
+
+function startClassicGame() {
+  game.type = 'classic';
+  game.message = 'Choose your fighter!';
+  
+}
