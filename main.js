092bec6d786d10b549c9c6f50594b37239bf7c53
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-08-07 11:52:03 -0700

    Grab mode buttons and win counters

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
+const game = new Game;
 const classicBtn = document.querySelector('.js-classic-mode');
 const advancedBtn = document.querySelector('.js-advanced-mode');
+
+let humanWinsCounter = document.querySelector('.js-human-wins-counter');
+let computerWinsCounter = document.querySelector('.js-computer-wins-counter');
