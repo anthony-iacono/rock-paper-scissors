bb9e4b66c2b73fbe73e164e381bb3b04cd6152a8
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-08-07 17:38:37 -0700

    Add fight function using alt text

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
 const game = new Game;
 const classicBtn = document.querySelector('.js-classic-btn');
 const advancedBtn = document.querySelector('.js-advanced-btn');
+const modeSection = document.querySelector('.js-mode-section');
+const arsenal = document.querySelector('.arsenal');
+const rock = document.querySelector('.rock')
+const paper = document.querySelector('.paper')
+const scissors = document.querySelector('.scissors')
+const spock = document.querySelector('.spock')
+const lizard = document.querySelector('.lizard')
 
-let humanWinsCounter = document.querySelector('.js-human-wins-counter');
-let computerWinsCounter = document.querySelector('.js-computer-wins-counter');
 
-humanWinsCounter.innerText = game.human.wins;
-computerWinsCounter.innerText = game.computer.wins;
+let humanCounter = document.querySelector('.js-human-counter');
+let computerCounter = document.querySelector('.js-computer-counter');
+
+humanCounter.innerText = game.human.wins;
+computerCounter.innerText = game.computer.wins;
 
 classicBtn.addEventListener('click', startClassicGame);
+arsenal.addEventListener('click', function() {
+  fight(event);
+});
 
 function startClassicGame() {
   game.type = 'classic';
   game.message = 'Choose your fighter!';
+  modeSection.classList.add('hidden');
+  arsenal.classList.remove('hidden');
+}
 
+function fight(event) {
+  console.log(event.target.alt);
+  game.human.takeTurn(event.target.alt);
+  game.computer.takeTurn();
 }
