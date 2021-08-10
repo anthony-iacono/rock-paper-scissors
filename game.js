901e90d0a84e1e22a84cdd2659b0b581f0355878
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-08-09 18:30:14 -0700

    Apply SRP to game.checkForWin

--- game.js
+++ game.js
@@ -7,16 +7,10 @@ class Game {
   }
 
   checkForWin() {
-    const draw = this.human.weapon === this.computer.weapon;
-    const humanWon =
-    (this.human.weapon === 'rock' && this.computer.weapon === 'scissors')
-    || (this.human.weapon === 'paper' && this.computer.weapon === 'rock')
-    || (this.human.weapon === 'scissors' && this.computer.weapon === 'paper');
-
-    if (draw) {
+    if (this.draw()) {
       this.message = '🤓 It\'s a draw! 💻'
       return 'draw';
-    } else if (humanWon) {
+    } else if (this.humanWon()) {
       this.human.wins++;
       this.human.saveWinsToStorage();
       this.message = '🤓 Human won this round! 🤓'
@@ -25,5 +19,48 @@ class Game {
       this.computer.saveWinsToStorage();
       this.message = '💻 Computer won this round! 💻';
     }
+  }
+
+  draw() {
+    return this.human.weapon === this.computer.weapon;
+  }
+
+  humanWon() {
+    return this.choseRock() || this.chosePaper() || this.choseScissors() || this.choseSpock() || this.choseLizard();
+  }
+
+  choseRock() {
+    return this.human.weapon === 'rock' && (
+      this.computer.weapon === 'scissors'
+      || this.computer.weapon === 'lizard'
+    );
+  }
+
+  chosePaper() {
+    return this.human.weapon === 'paper' && (
+      this.computer.weapon === 'rock'
+      || this.computer.weapon === 'spock'
+    );
+  }
+
+  choseScissors() {
+    return this.human.weapon === 'scissors' && (
+      this.computer.weapon === 'paper'
+      || this.computer.weapon === 'lizard'
+    );
+  }
+
+  choseSpock() {
+    return this.human.weapon === 'spock' && (
+      this.computer.weapon === 'rock'
+      || this.computer.weapon === 'scissors'
+    );
+  }
+
+  choseLizard() {
+    return this.human.weapon === 'lizard' && (
+      this.computer.weapon === 'paper'
+      || this.computer.weapon === 'spock'
+    );
   }
 }
